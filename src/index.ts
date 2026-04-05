eadee7c9b041dafb64cb56216af2ac11773ab139
azaletnev <zaletnev@realx.tech>
2026-04-05 19:55:07 +0300

    fix paths

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as devRunning } from './devRunning';
-export { default as viteIdeHelperGenerate } from './viteIdeHelperGenerate';
-export { default as pseudoConfig } from './pseudoConfig';
+export { default as devRunning } from 'devRunning';
+export { default as viteIdeHelperGenerate } from 'viteIdeHelperGenerate';
+export { default as pseudoConfig } from 'pseudoConfig';
